a0dd7158b92d2d89255003d0526150394ebceb96
Sebastien Ponce <Sebastien.Ponce@cern.ch>
2018-04-20 15:16:04 +0200

    Handle all types of rotations in extract

--- ATLAS/extractPart.py
+++ ATLAS/extractPart.py
@@ -2,7 +2,41 @@ import sys
 import numpy
 from numpy.linalg import inv
 
-rotate180 = (0,0,0,-1,0,0,0,1,0,0,0,-1)
+def applyRotation(pInPiece, t):
+    xp, yp, zp, ap, bp, cp, dp, ep, fp, gp, hp, ip = pInPiece
+    x, y, z, a, b, c, d, e, f, g, h, i = t
+    matp = numpy.matrix([[ap,bp,cp], [dp,ep,fp], [gp,hp,ip]])
+    mat = numpy.matrix([[a,b,c], [d,e,f], [g,h,i]])
+    matr = mat*matp
+    matr = matr.tolist()
+    ar,br,cr = matr[0]
+    dr,er,fr = matr[1]
+    gr,hr,ir = matr[2]
+    return (xp, yp, zp, ar, br, cr, dr, er, fr, gr, hr, ir)
+
+rotate90X = (0,0,0, 1,0,0,  0,0,-1, 0,1,0)
+rotate90Y = (0,0,0, 0,0,-1, 0,1,0,  1,0,0)
+rotate90Z = (0,0,0, 0,-1,0, 1,0,0,  0,0,1)
+
+rotate180X = (0,0,0,1,0,0,0,-1,0,0,0,-1)
+rotate180Y = (0,0,0,-1,0,0,0,1,0,0,0,-1)
+rotate180Z = (0,0,0,-1,0,0,0,-1,0,0,0,1)
+
+rotate270X = (0,0,0, 1,0,0, 0,0,1,  0,-1,0)
+rotate270Y = (0,0,0, 0,0,1, 0,1,0, -1,0,0)
+rotate270Z = (0,0,0, 0,1,0, -1,0,0, 0,0,1)
+
+all180Rotations = [rotate180X, rotate180Y, rotate180Z]
+allRotations = [rotate90X, rotate90Y, rotate90Z,
+                rotate180X, rotate180Y, rotate180Z,
+                rotate270X, rotate270Y, rotate270Z,
+                    applyRotation(rotate180X, rotate90Y), applyRotation(rotate180X, rotate270Y),
+                    applyRotation(rotate180X, rotate90Z), applyRotation(rotate180X, rotate270Z),
+                    applyRotation(rotate180Y, rotate90X), applyRotation(rotate180Y, rotate270X),
+                    applyRotation(rotate180Y, rotate90Z), applyRotation(rotate180Y, rotate270Z),
+                    applyRotation(rotate180Z, rotate90X), applyRotation(rotate180Z, rotate270X),
+                    applyRotation(rotate180Z, rotate90Y), applyRotation(rotate180Z, rotate270Y)]
+    
 def rounded(s):
     f = float(s)
     i = round(f)
@@ -69,6 +103,18 @@ def applyTransform(pInPiece, t):
     gr,hr,ir = matr[2]
     return (xr, yr, zr, ar, br, cr, dr, er, fr, gr, hr, ir)
 
+def applyRotation(pInPiece, t):
+    xp, yp, zp, ap, bp, cp, dp, ep, fp, gp, hp, ip = pInPiece
+    x, y, z, a, b, c, d, e, f, g, h, i = t
+    matp = numpy.matrix([[ap,bp,cp], [dp,ep,fp], [gp,hp,ip]])
+    mat = numpy.matrix([[a,b,c], [d,e,f], [g,h,i]])
+    matr = mat*matp
+    matr = matr.tolist()
+    ar,br,cr = matr[0]
+    dr,er,fr = matr[1]
+    gr,hr,ir = matr[2]
+    return (xp, yp, zp, ar, br, cr, dr, er, fr, gr, hr, ir)
+
 # piece = (1,1,1, 1,0,0,0,1,0,0,0,1)
 # model = (-1,-1,-1, -1,0,0,0,-1,0,0,0,1)
 # print 'piece :', piece
@@ -93,15 +139,12 @@ def find(item, l):
             return True
     return False
 
-def simplifyModel(piece, model, t, tsym):
-    for b in piece:
-        for col, placeInPiece in piece[b]:
-            placeInModel = applyTransform(placeInPiece, t)
-            placeInModelSym = applyTransform(placeInPiece, tsym)
-            for col, a in model[b]:
-                if closeEnough(placeInModel, a) or closeEnough(placeInModelSym, a):
-                    model[b].remove((col, a))
-                    break            
+def simplifyModel(piecesToRemove, model):
+    for b, pcol, placeInModel in piecesToRemove:
+        for col, a in model[b]:
+            if closeEnough(placeInModel, a):
+                model[b].remove((col, a))
+                break            
 
 def findPieceInModel(pieceName, piece, model):
     # find the brick in piece that is the least common in the model
@@ -122,29 +165,40 @@ def findPieceInModel(pieceName, piece, model):
     transforms = []
     for col, placeInPiece in piece[bestBrick]:
         for col2, placeInModel in model[bestBrick]:
-            transforms.append((computeTransform(placeInPiece, placeInModel),
-                computeTransform(applyTransform(rotate180, placeInPiece), placeInModel)))
-    print transforms
+            transforms.append((computeTransform(placeInPiece, placeInModel), (bestBrick, col, placeInPiece)))
+            for rot in all180Rotations:
+                rotPiece = applyTransform(rot, placeInPiece)
+                transforms.append((computeTransform(rotPiece, placeInModel), (bestBrick, col, rotPiece)))
     # For each transform, go through all bricks of the piece and
     # check we find the corresponding one in model
     res = []
-    for t, tsym in transforms:
+    for t, pieceToRemove in transforms:
         #print 'Trying transform', t, tsym
         ok = True
+        piecesToRemove = [pieceToRemove]
+        print pieceToRemove
         for b in piece:
             if not ok: break
             for col, placeInPiece in piece[b]:
-                #print 'going for', b, placeInPiece
                 placeInModel = applyTransform(placeInPiece, t)
-                placeInModelSym = applyTransform(placeInPiece, tsym)
-                #print placeInPiece, placeInModel, placeInModelSym
-                #print model[b]
-                # also try with 180 degres rotation of the original piece, in case it's symetric
-                if not find((col, placeInModel), model[b]) and not find((col, placeInModelSym), model[b]):
-                    ok = False
-                    break
+                if find((col, placeInModel), model[b]):
+                    piecesToRemove.append((b, col, placeInModel))
+                else:
+                    # try different rotation of the piece checked
+                    found = False
+                    for rot in allRotations:
+                        nplaceInPiece = applyRotation(placeInPiece, rot)
+                        placeInModel = applyTransform(nplaceInPiece, t)
+                        if find((col, placeInModel), model[b]):
+                            piecesToRemove.append((b, col, placeInModel))
+                            found = True
+                            break
+                    if not found:
+                        # we've tried everything...
+                        ok = False
+                        break
         if ok:
-            simplifyModel(piece, model, t, tsym)
+            simplifyModel(piecesToRemove, model)
             res.append(list(t) + [pieceName])
             print 'Found piece in model with transform %s' % (str(t))
     return res
